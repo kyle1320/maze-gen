6ba00ad150d1b2623a30e9665974cd88c8fb487f
Kyle Cutler <kyle1320@gmail.com>
2015-11-05 13:34:40 -0500

    commit to git

---
+++ MazeGen_golf.py
@@ -0,0 +1,10 @@
+from random import *
+r,l,o,w,h=choice,list,(0,0),25,25
+a=lambda(i,j):{(x,y)for x,y in[(i+1,j),(i-1,j),(i,j+1),(i,j-1)]if(0<=x<w)&(0<=y<h)};v,e,p={o},a(o),{0}
+while e:
+ c=r(l(e));n=a(c);b,n=r(l(v&n)),n-v
+ while 1:
+  p|={(b[0]+c[0],b[1]+c[1])};e^={c};v|={c};n=a(c)-v
+  if not n or.05>random():break
+  b,c=c,r(l(n));e|=n
+print"\n@ ".join(["@ "*(w*2+1)]+["".join([("@ ","  ")[x%2+y%2<1 or{(x,y)}<p and y<h*2-1]for x in range(w*2)])for y in range(h*2)])
---
+++ README.md
@@ -0,0 +1,13 @@
+Maze Generation
+==============
+
+My own custom maze generation algorithm implemented in Python
+--------------
+
+This is a Python implementation of a maze generation algorithm of my own design.
+
+I also included a code-golfed version of the algorithm, because why not.
+
+# The Algorithm
+
+  * _check back soon!_
